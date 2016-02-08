7f616101d589aa0dd986050c60fa9305a4af9334
Thomas Woodfield <thomas.w02@icloud.com>
2016-02-08 18:31:54 +1300

    First Commit

--- Colours/ViewController.swift
+++ Colours/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueImage: UIImageView!
+    @IBOutlet weak var redImage: UIImageView!
+    @IBOutlet weak var yellowImage: UIImageView!
+    @IBOutlet weak var greenImage: UIImageView!
+    @IBOutlet weak var blueSwitch: UISwitch!
+    @IBOutlet weak var redSwitch: UISwitch!
+    @IBOutlet weak var yellowSwitch: UISwitch!
+    @IBOutlet weak var greenSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func changeBlue(sender: AnyObject) {
+        if blueSwitch.on {
+            blueImage.hidden = false
+        } else {
+            blueImage.hidden = true
+        }
+    }
 
+    @IBAction func changeRed(sender: AnyObject) {
+        if redSwitch.on {
+            redImage.hidden = false
+        } else {
+            redImage.hidden = true
+        }
+
+    }
+
+    @IBAction func changeYellow(sender: AnyObject) {
+        if yellowSwitch.on {
+            yellowImage.hidden = false
+        } else {
+            yellowImage.hidden = true
+        }
+
+    }
+    @IBAction func changeGreen(sender: AnyObject) {
+        if greenSwitch.on {
+            greenImage.hidden = false
+        } else {
+            greenImage.hidden = true
+        }
+
+    }
+    
 }
 
